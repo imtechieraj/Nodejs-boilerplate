1be39ef7c7b9b52b16de4558927ed97d18aa63ef
Rajkumar <hithisisrajkumar@gmail.com>
2019-10-06 01:58:34 +0530

    All routes are maintaining routes folder

---
+++ Routes/users.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const app = express.Router();
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+module.exports = app;
--- app.js
+++ app.js
@@ -1,7 +1,10 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+// Maintain the all the routes in Routes folder
+app.use("/user",require('./Routes/users'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//Binds and listens for connections on the specified host and port
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
